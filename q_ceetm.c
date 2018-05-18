ca49b40f650377cc2c9f970ec0cd27546234731f
Camelia Groza <camelia.groza@nxp.com>
2018-05-18 15:32:35 +0300

    ceetm: add a DPAA version detector call
    Read the System Version Register exported in sysfs to detect the SoC
    DPAA version (DPAA 1 or DPAA 2).
    
    Signed-off-by: Camelia Groza <camelia.groza@nxp.com>

--- q_ceetm.c
+++ q_ceetm.c
@@ -39,6 +39,39 @@
 #include "dpaa1_ceetm.h"
 #include "dpaa2_ceetm.h"
 
+/* DPAA SoC identifier.
+ * If this is not available, assume the board is DPAA2.
+ */
+#define DPAA_SOC_ID_FILE	"/sys/devices/soc0/soc_id"
+
+#define SVR_LS1043A_FAMILY	0x87920000
+#define SVR_LS1046A_FAMILY	0x87070000
+#define SVR_MASK		0xffff0000
+
+enum dpaa_version {
+	DPAA_1,
+	DPAA_2,
+};
+
+static enum dpaa_version detect_dpaa_version(void)
+{
+	FILE *svr_file = fopen(DPAA_SOC_ID_FILE, "r");
+	unsigned int svr_ver, dpaa_svr_family = 0;
+
+	if (svr_file) {
+		if (fscanf(svr_file, "svr:%x", &svr_ver) > 0)
+			dpaa_svr_family = svr_ver & SVR_MASK;
+		fclose(svr_file);
+	}
+
+	switch (dpaa_svr_family) {
+	case SVR_LS1043A_FAMILY:
+	case SVR_LS1046A_FAMILY:
+		return DPAA_1;
+	default:
+		return DPAA_2;
+	}
+}
 
 static int ceetm_parse_qopt(struct qdisc_util *qu, int argc, char **argv,
 		struct nlmsghdr *n)
